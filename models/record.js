16960f5ab2fb0afeae0a12431a8523a661ed54be
Marc <g40419@gmail.com>
2020-05-23 17:16:38 +0800

    feat: add body-parser

---
+++ models/record.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Record = new Schema({
+  amount : {
+    type: Number,
+    require: true,
+  },
+  title : {
+    type: String,
+    require: true
+  },
+  date : {
+    type: String
+  },
+  category : {
+    type: String
+  },
+  account : {
+    type: String,
+    default: "現金"
+  },
+  totalAmount : {
+    type : Number
+  }
+
+})  
+
+module.exports = mongoose.model('Record', Record)
